ebe6ff0073256306d99c306e3bed9889de565052
kimmyunghoon <kl951995@gmail.com>
2022-04-17 17:27:06 +0900

    [수정] atom 값을 사용해서 fetch 요청하기

--- src/features/memo/recoil/atoms.ts
+++ src/features/memo/recoil/atoms.ts
@@ -5,6 +5,16 @@ export const memoListState = atom({
     default: [], // default value (aka initial value)
 });
 
+export const memoCollection = atom({
+    key: 'memoCollection', // unique ID (with respect to other atoms/selectors)
+    default: "memos", // default value (aka initial value)
+});
+export const memoType = atom({
+    key: 'memoType', // unique ID (with respect to other atoms/selectors)
+    default: "common", // default value (aka initial value)
+});
+
+
 export const memoTitle = atom({
     key: 'memoTitle', // unique ID (with respect to other atoms/selectors)
     default: "none title", // default value (aka initial value)
--- src/features/memo/recoil/selectors.ts
+++ src/features/memo/recoil/selectors.ts
@@ -1,5 +1,5 @@
 import {selector} from "recoil";
-import {memoListState} from "./atoms";
+import {memoCollection, memoListState, memoType} from "./atoms";
 
 export const memoListSelector = selector({
     key: 'memoListSelector', // unique ID (with respect to other atoms/selectors)
@@ -7,11 +7,13 @@ export const memoListSelector = selector({
         console.log("memoListSelector")
         // atom 가져오기
          // let list = get(memoListState)
+        let collection = get(memoCollection)
+        let type = get(memoType)
          //    return get(memoListState)
         // Todo
         //  사용자가 추가되면 해당 사용자 키값에 해당하는 메모를 가져오는 방법으로 변경해야할지도 모르겟음.
         //  서버에서 리스트 가져오기
-        return await fetch("http://localhost:8080/firestore/memos/common", {
+        return await fetch(`http://localhost:8080/firestore/${collection}/${type}`, {
             method:"GET",
             mode: 'cors', // no-cors, cors, *same-origin
             headers: {
