5089db52463c3fde13337b78a9803398fab4fa16
kimmyunghoon <kl951995@gmail.com>
2022-04-22 10:14:58 +0900

    [수정] fetch 함수 구현(type wrapping)

---
+++ src/features/memo/memoActions.ts
@@ -0,0 +1,47 @@
+import {MemoType} from "./type/interface";
+import {MemoInfo} from "./type/type_class";
+
+
+export const modifyMemo = async (collection: string, type: string, memo: MemoType): Promise<MemoType[]> =>
+    await fetch(`http://localhost:8080/firestore/${collection}/${type}`, {
+        method: "POST",
+        mode: 'cors', // no-cors, cors, *same-origin
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id: memo.Id, title: memo.Title, contents: memo.Contents}),
+    }).then(function (response) {
+        if (!response.ok) {
+            throw new Error('Bad status code from server.');
+        }
+        return response.json();
+    }).then(function (myJson) {
+
+        return myJson.message
+    }).catch((error) => {
+        console.error('실패:', error);
+        return []
+    })
+
+
+
+export const retrieveMemo = async (collection: string): Promise<MemoType[]> =>
+    await fetch(`http://localhost:8080/firestore/${collection}/common`, {
+        method: "GET",
+        mode: 'cors', // no-cors, cors, *same-origin
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(function (response) {
+        if (!response.ok) {
+            throw new Error('Bad status code from server.');
+        }
+        return response.json();
+    }).then(function (myJson) {
+        console.log("current ",myJson.message)
+        return myJson.message
+    }).catch((error) => {
+        console.error('실패:', error);
+        return []
+    })
+
--- src/features/memo/recoil/selectors.ts
+++ src/features/memo/recoil/selectors.ts
@@ -6,31 +6,14 @@ import {
 } from "./atoms";
 import {ApiCommand, MemoType} from "../type/interface";
 import {MemoInfo} from "../type/type_class";
+import {modifyMemo, retrieveMemo} from "../memoActions";
 //
 
 export const currentMemoListSelector = selector<MemoType[]>({
     key: 'currentMemoListSelector', // unique ID (with respect to other atoms/selectors)
     get: async ({get}) => {
         let collection = get(memoCollection)
-
-        return await fetch(`http://localhost:8080/firestore/${collection}/common`, {
-            method: "GET",
-            mode: 'cors', // no-cors, cors, *same-origin
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(function (response) {
-            if (!response.ok) {
-                throw new Error('Bad status code from server.');
-            }
-            return response.json();
-        }).then(function (myJson) {
-            console.log("current ",myJson.message)
-            return myJson.message
-        }).catch((error) => {
-            console.error('실패:', error);
-            return []
-        })
+        return retrieveMemo(collection)
     },
     set: ({get, set, reset}, newValue) => {
         console.log(newValue)
@@ -44,29 +27,16 @@ export const updateMemoListSelector = selector<ApiCommand>({
         console.log("updateMemoListSelector")
         const memoData : ApiCommand = get(memoDataState)
         const collection = get(memoCollection)
+        let type = memoData["type"]
         let values:ApiCommand  = {
             type:"result",
             data: new MemoInfo()
         }
-        if("type" in memoData && "data" in memoData) {
-            let type = memoData["type"]
+        if("type" in memoData && "data" in memoData && type !=="none") {
+
             let memo :MemoType  = memoData['data']
 
-            values.list=  await fetch(`http://localhost:8080/firestore/${collection}/${type}`, {
-                method: "POST",
-                mode: 'cors', // no-cors, cors, *same-origin
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({id: memo.Id, title: memo.Title, contents: memo.Contents}),
-            }).then(function (response) {
-                if (!response.ok) {
-                    throw new Error('Bad status code from server.');
-                }
-                return response.json();
-            }).then(function (myJson) {
-                  return myJson.message
-            });
+            values.list =  await modifyMemo(collection,type,memo);
         }
         return values
     },
