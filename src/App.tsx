2f20c2296a48e2730e406eff5eabe40dfe55e03d
kimmyunghoon <kl951995@gmail.com>
2022-04-25 14:48:32 +0900

    [수정] react-router typescript 버전 사용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,19 @@ import {
 } from 'recoil';
 import MemoPage from "./pages/memo/MemoPage";
 import ExamplesPage from "./pages/examples/ExamplesPage";
+import { Route, Routes} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 const App = () => {
   return (
       <RecoilRoot>
-        <MemoPage />
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" >
+                      <Route  path={'memo'} element={<MemoPage/>} />
+                      <Route  path={'e'}  element={<ExamplesPage/>} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
       </RecoilRoot>
   );
 }
